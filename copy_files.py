90c25bf26778d3e47546424695af67e49d7cca5c
Harshdeep Singh Tomar <harshdeep.tomar@gmail.com>
2024-09-07 21:00:10 +0530

    test7

--- copy_files.py
+++ copy_files.py
@@ -1,4 +1,7 @@
 import os
 print("Script loaded")
-dir=os.getcwd()
-print(dir)
+os.chdir("/home/runner/work/ota/ota")
+
+dir_list=os.listdir("/home/runner/work/ota/ota/ota/devices")
+print(dir_list)
+
