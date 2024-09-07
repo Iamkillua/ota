bb2f5818ffb3bef694a9f767614cea2af1f9b834
Harshdeep Singh Tomar <harshdeep.tomar@gmail.com>
2024-09-07 21:13:40 +0530

    test10

--- copy_files.py
+++ copy_files.py
@@ -3,7 +3,7 @@ print("Script loaded")
 os.chdir("/home/runner/work/ota/ota")
 file_list=os.listdir("/home/runner/work/ota/ota/ota/devices")
 dir_list=[]
-for i in dir_list:
+for i in file_list:
     j=i.replace(".json","")
     print(i)
     print(j)
