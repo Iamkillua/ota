c6221566a9ecc12d45c90baf34f49cde45143f90
Harshdeep Singh Tomar <harshdeep.tomar@gmail.com>
2024-09-07 21:12:37 +0530

    test9

--- copy_files.py
+++ copy_files.py
@@ -5,5 +5,8 @@ file_list=os.listdir("/home/runner/work/ota/ota/ota/devices")
 dir_list=[]
 for i in dir_list:
     j=i.replace(".json","")
+    print(i)
+    print(j)
     dir_list.append(j)
+
 print(dir_list)
