e131996e45fea619f9825caee5ec8a1ae55e77b2
Harshdeep Singh Tomar <harshdeep.tomar@gmail.com>
2024-09-07 19:50:49 +0530

    Added python script and fixed a few bugs causing the build to fail.

--- copy_files.py
+++ copy_files.py
@@ -1,1 +1,12 @@
+import os
 print("Script loaded")
+os.chdir("/home/runner/work/ota/ota")
+file_list=os.listdir("/home/runner/work/ota/ota/ota/devices")
+dir_list=[]
+for i in file_list:
+    j=i.replace(".json","")
+    print(i)
+    print(j)
+    dir_list.append(j)
+
+print(dir_list)
